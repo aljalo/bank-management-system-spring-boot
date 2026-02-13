1c4d53dbd9be9c30dde95abe47742eac21f80407
jalo <ali.aljalo@gmail.com>
2026-02-13 22:31:21 +0300

    feat: seed default roles (ROLE_USER, ROLE_ADMIN) at application startup

--- src/main/java/org/wscict/bank/BankApplication.java
+++ src/main/java/org/wscict/bank/BankApplication.java
@@ -1,8 +1,13 @@
 package org.wscict.bank;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.wscict.bank.auth.Role;
+import org.wscict.bank.auth.RoleRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -11,5 +16,18 @@ public class BankApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(BankApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner run(RoleRepository roleRepository) {
+        return args -> {
+
+            if (roleRepository.findByName("ROLE_USER").isEmpty()) {
+                roleRepository.save(new Role("ROLE_USER"));
+            }
+
+            if (roleRepository.findByName("ROLE_ADMIN").isEmpty()) {
+                roleRepository.save(new Role("ROLE_ADMIN"));
+            }
+        };
     }
 }
