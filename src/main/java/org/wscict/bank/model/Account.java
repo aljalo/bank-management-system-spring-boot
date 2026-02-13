eb34e4e0c846c0fcbd4683575e8e7a578f48b0e5
jalo <ali.aljalo@gmail.com>
2026-02-13 12:06:38 +0300

    feat: add JPA auditing with createdAt and updatedAt fields

--- src/main/java/org/wscict/bank/model/Account.java
+++ src/main/java/org/wscict/bank/model/Account.java
@@ -1,19 +1,35 @@
 package org.wscict.bank.model;
 
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "accounts")
+@EntityListeners(AuditingEntityListener.class)
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String ownerName;
+
     private double balance;
+
     //private String status;
     @Enumerated(EnumType.STRING)
     private AccountStatus accountStatus;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 
     public Account() {
     }
@@ -25,7 +41,7 @@ public class Account {
         this.accountStatus = AccountStatus.ACTIVE;
     }
 
-    //
+
     public Long getId() {
         return id;
     }
@@ -47,15 +63,6 @@ public class Account {
     public void setBalance(double balance) {
         this.balance = balance;
     }
-
-    //
-//    public String getStatus() {
-//        return status;
-//    }
-//
-//    public void setStatus(String status) {
-//        this.status = status;
-//    }
 
     //
     public AccountStatus getAccountStatus() {
