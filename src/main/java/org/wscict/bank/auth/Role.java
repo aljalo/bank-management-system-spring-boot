7a4669c60453a335cf9ce3fb4980d804a837d95a
jalo <ali.aljalo@gmail.com>
2026-02-14 11:49:51 +0300

    feat(auth): implement User and Role entities with addRole method

--- src/main/java/org/wscict/bank/auth/Role.java
+++ src/main/java/org/wscict/bank/auth/Role.java
@@ -13,7 +13,8 @@ public class Role {
     @Column(nullable = false, unique = true)
     private String name;
 
-    public Role() {}
+    public Role() {
+    }
 
     public Role(String name) {
         this.name = name;
@@ -25,5 +26,18 @@ public class Role {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return name.equals(role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
     }
 }
